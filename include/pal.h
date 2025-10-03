c5d7ee42969b3bbe267fe34944e1a009e37dd37c
Mirko Lana <lana.mirko@icloud.com>
2025-10-03 16:50:29 +0200

    feat: backup commit

--- include/pal.h
+++ include/pal.h
@@ -13,15 +13,50 @@
 #ifndef PAL_H
 #define PAL_H
 
+#include "ring-buffer.h"
+
 #include <stddef.h>
 #include <stdint.h>
+
+#ifndef PAL_MESSAGE_BUFFER_SIZE
+/*!
+ * \brief           The default message buffer size.
+ */
+#define PAL_MESSAGE_BUFFER_SIZE 0xFF
+#endif /*! PAL_MESSAGE_BUFFER_SIZE */
 
 /*!
  * \brief           Enumeration with all possible return codes of the library.
  */
 enum PalReturnCode {
-    PAL_RC_OK,      /*!< Everything is fine */
-    PAL_RC_NULL_PTR /*!< Unexpected NULL pointer detected */
+    PAL_RC_OK,       /*!< Everything is fine */
+    PAL_RC_NULL_PTR, /*!< Unexpected NULL pointer detected */
+};
+
+/*!
+ * \brief           Type definition for a function pointer used to send data.
+ *
+ * \param[in]       raw_data: Pointer to the data to be sent.
+ * \param[in]       size: Size of the data buffer in bytes.
+ * \return          0 on success, -1 otherwise.
+ */
+typedef int (*pal_send_raw_fn)(const uint8_t *raw_data, size_t size);
+
+/*!
+ * \brief           Type definition for a function pointer used to receive data.
+ *
+ * \param[out]      raw_data: Pointer to the data to be received.
+ * \param[out]      size: Size of the data buffer in bytes.
+ * \return          The number of bytes received on success, -1 otherwise.
+ */
+typedef int (*pal_recv_raw_fn)(uint8_t *raw_data, size_t size);
+
+/*!
+ * \brief           A structure representing a message.
+ */
+struct PalMessage {
+    char raw_data[PAL_MESSAGE_BUFFER_SIZE]; /*!< Raw data buffer */
+    size_t size;                            /*!< Data size */
 };
 
 /*!
@@ -31,6 +66,10 @@ enum PalReturnCode {
  * \attention       This structure should not be used directly.
  */
 struct PalHandler {
+    pal_send_raw_fn send;
+    pal_recv_raw_fn recv;
+    RingBufferHandler_t tx_buffer;
+    RingBufferHandler_t rx_buffer;
 };
 
 #endif /*! PAL_H */
