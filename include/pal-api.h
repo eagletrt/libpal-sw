f022b761fc3e0530a24414615210e38ec8bb9c77
Mario-Mazzara <115786855+Mario-Mazzara@users.noreply.github.com>
2025-10-05 19:27:44 +0200

    feat: library headers

--- include/pal-api.h
+++ include/pal-api.h
@@ -7,14 +7,86 @@
  * \brief           Peripheral Abstraction Layer (PAL) library, suited for all
  *                  embedded devices.
  *
- * \details         TODO
+ * \details         This library provides a set of functions and data structures
+ *                  to abstract the communication with peripherals in a device.
+ *                  It's designed to be platform-independent so a driver must be implemented
+ *                  by the user to provide the necessary functionality.
+ *
+ * \warning         The dynamically allocated memory will not be deallocated automatically but has
+ *                  to be freed by using the arena allocator.
  */
 
 #ifndef PAL_API_H
 #define PAL_API_H
 
 #include "pal.h"
+#include "arena-allocator.h"
 
-enum PalReturnCode pal_api_init(struct PalHandler *hpal);
+/*!
+ * \brief           Initialize the P.A.L. handler.
+ *
+ * \param[out]      hpal: A pointer to the handler to initialize.
+ * \param[in]       send: Send function
+ * \param[in]       recv: Receive function
+ * \param[in]       driver_ctx: Internal driver context
+ * \param[in]       enter_cs: Enter critical section function
+ * \param[in]       exit_cs: Exit critical section function
+ * \param[in]       app_cb: Application callback function
+ * \param[in]       arena: Arena allocator handler (already initialized)
+ * \return          PAL_RC_OK on success, and error code otherwise:
+ *                      - PAL_RC_NULL_PTR
+ */
+enum PalReturnCode pal_api_init(
+    struct PalHandler *hpal,
+    pal_recv_raw_fn recv,
+    pal_send_raw_fn send,
+    void *driver_ctx,
+    void (*enter_cs)(void),
+    void (*exit_cs)(void),
+    void (*app_cb)(void),
+    ArenaAllocatorHandler_t *arena
+);
+
+/*!
+ * \brief           Function called by the driver inside the ISR routine to give the message data to PAL
+ *
+ * \param[out]      hpal: PAL handler
+ * \param[out]      raw_data: Raw data buffer provided by the driver
+ * \param[in]       size: Size of the raw data buffer
+ * \param[in]       deserialize: Deserialization function
+ * \return          PAL_RC_OK on success, an error code otherwise.
+ */
+enum PalReturnCode pal_api_drv_recv_cb(struct PalHandler *hpal, uint8_t *raw_data, size_t size, pal_deserialize_fn deserialize);
+
+/*!
+ * \brief           TBD
+ *
+ * \param[out]      hpal:
+ * \param[out]      raw_data:
+ * \param[in]       size:
+ * \param[in]       serialize:
+ * \return          PAL_RC_OK on success, an error code otherwise.
+ */
+// enum PalReturnCode pal_api_drv_send_cb(struct PalHandler *hpal, uint8_t *raw_data, size_t size, pal_serialize_fn serialize);
+
+/*!
+ * \brief           Function to send data using the provided handler and serialization function
+ *
+ * \param[out]      hpal:
+ * \param[in]       data:
+ * \param[in]       serialize:
+ * \return          PAL_RC_OK on success, an error code otherwise.
+ */
+enum PalReturnCode pal_api_send(struct PalHandler *hpal, const void *data, pal_serialize_fn serialize);
+
+/*!
+ * \brief           TBD
+ *
+ * \param[out]      hpal:
+ * \param[out]      data:
+ * \param[in]       deserialize:
+ * \return          PAL_RC_OK on success, an error code otherwise.
+ */
+// enum PalReturnCode pal_api_recv(struct PalHandler *hpal, void *data, pal_deserialize_fn deserialize);
 
 #endif /*! PAL_API_H */
--- include/pal.h
+++ include/pal.h
@@ -7,7 +7,13 @@
  * \brief           Peripheral Abstraction Layer (PAL) library, suited for all
  *                  embedded devices.
  *
- * \details         TODO
+ * \details         This library provides a set of functions and data structures
+ *                  to abstract the communication with peripherals in a device.
+ *                  It's designed to be platform-independent so a driver must be implemented
+ *                  by the user to provide the necessary functionality.
+ *
+ * \warning         The dynamically allocated memory will not be deallocated automatically but has
+ *                  to be freed by using the arena allocator.
  */
 
 #ifndef PAL_H
@@ -25,12 +31,30 @@
 #define PAL_MESSAGE_BUFFER_SIZE 0xFF
 #endif /*! PAL_MESSAGE_BUFFER_SIZE */
 
+
+#ifndef PAL_TX_BUFFER_SIZE
+/*!
+ * \brief           TDB
+ */
+#define PAL_TX_BUFFER_SIZE 0xA
+#endif /*! PAL_TX_BUFFER_SIZE */
+
+#ifndef PAL_RX_BUFFER_SIZE
+/*!
+ * \brief           TBD
+ */
+#define PAL_RX_BUFFER_SIZE PAL_TX_BUFFER_SIZE
+#endif /*! PAL_RX_BUFFER_SIZE */
+
 /*!
  * \brief           Enumeration with all possible return codes of the library.
  */
 enum PalReturnCode {
-    PAL_RC_OK,       /*!< Everything is fine */
-    PAL_RC_NULL_PTR, /*!< Unexpected NULL pointer detected */
+    PAL_RC_OK,         /*!< Everything is fine */
+    PAL_RC_NULL_PTR,   /*!< Unexpected NULL pointer detected */
+    PAL_RC_WRONG_SIZE, /*!< Size mismatch */
+    PAL_RC_IO_ERR,     /*!< Generic I/O error */
+    PAL_RC_BUFF_FULL,  /*!< Rx or Tx buffer is full */
 };
 
 /*!
@@ -46,17 +70,35 @@ typedef int (*pal_send_raw_fn)(const uint8_t *raw_data, size_t size);
  * \brief           Type definition for a function pointer used to receive data.
  *
  * \param[out]      raw_data: Pointer to the data to be received.
- * \param[out]      size: Size of the data buffer in bytes.
+ * \param[in]       size: Size of the data buffer in bytes.
  * \return          The number of bytes received on success, -1 otherwise.
  */
 typedef int (*pal_recv_raw_fn)(uint8_t *raw_data, size_t size);
 
 /*!
+ * \brief           Type definition for a function pointer used to serialize data.
+ *
+ * \param[in]       data: Pointer to the data to be serialized
+ * \param[out]      size: Size of the buffer in bytes.
+ * \return          The number of bytes received on success, -1 otherwise.
+ */
+typedef int (*pal_serialize_fn)(const void *data, uint8_t *buff, size_t size);
+
+/*!
+ * \brief           Type definition for a function pointer used to deserialize data.
+ *
+ * \param[out]      data: Pointer to the data to be deserialized.
+ * \param[in]       size: Size of the buffer in bytes.
+ * \return          PAL_RC_OK on success, an error code otherwise.
+ */
+typedef enum PalReturnCode (*pal_deserialize_fn)(void *data, const uint8_t *buff, size_t size);
+
+/*!
  * \brief           A structure representing a message.
  */
 struct PalMessage {
-    char raw_data[PAL_MESSAGE_BUFFER_SIZE]; /*!< Raw data buffer */
-    size_t size;                            /*!< Data size */
+    uint8_t raw_data[PAL_MESSAGE_BUFFER_SIZE]; /*!< Raw data buffer */
+    size_t size;                               /*!< Data size */
 };
 
 /*!
@@ -68,8 +110,12 @@ struct PalMessage {
 struct PalHandler {
     pal_send_raw_fn send;
     pal_recv_raw_fn recv;
-    RingBufferHandler_t tx_buffer;
-    RingBufferHandler_t rx_buffer;
+    void *driver_ctx;
+    RingBufferHandler_t tx_buffer; // Owned by PAL
+    RingBufferHandler_t rx_buffer; // Owned by PAL
+    void (*enter_cs)(void);
+    void (*exit_cs)(void);
+    void (*app_cb)(void);  // to be called by pal_api_driver_receive_callback when data is received TODO: define the function signature
 };
 
 #endif /*! PAL_H */
